6da6d3749327c88f3258985650acb82ab246d48c
mdwhaley <michael.d.whaley@gmail.com>
2023-05-25 20:42:01 -0400

    Initial Commit

---
+++ DryTrails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DryTrails
+//
+//  Created by Michael Whaley on 5/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
